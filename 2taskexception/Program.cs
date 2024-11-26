a1cd438acd255d3e952c9cb4b28796b3699b3fcd
Kate Kumpanska <kate.kumpansky@gmail.com>
2024-11-26 10:26:30 +0200

    add regex and foreach with try-catch

--- 2taskexception/Program.cs
+++ 2taskexception/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace _2taskexception
@@ -14,6 +15,18 @@ namespace _2taskexception
             Console.Write("Enter directory: ");
             string dir = Console.ReadLine();
             string[] files = Directory.GetFiles(dir);
+            Regex regexExtForImage = new Regex("^((bmp)|(gif)|(jpe?g)|(png)|(tif)|(heic))$", RegexOptions.IgnoreCase);
+            foreach (string file in files)
+            {
+                try 
+                { 
+                
+                }
+                catch (Exception ex)
+                { 
+                
+                }
+            }
         }
 
     }
