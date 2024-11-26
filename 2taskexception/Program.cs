746867b53090e3e49455eab492cfd8b5d1899486
Kate Kumpanska <kate.kumpansky@gmail.com>
2024-11-26 10:22:08 +0200

    add class Files amd method ReadFiles

---
+++ 2taskexception/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2taskexception
+{
+    class Files
+    {
+        public static void ReadFiles()
+        {
+            Console.Write("Enter directory: ");
+            string dir = Console.ReadLine();
+            string[] files = Directory.GetFiles(dir);
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
