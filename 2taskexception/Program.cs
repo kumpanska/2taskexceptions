bb089aad597e4704e3fbb7370e44ad7839cdaf9a
Kate Kumpanska <kate.kumpansky@gmail.com>
2024-11-26 10:29:39 +0200

    add if regex match file extension and add methods for bitmap

--- 2taskexception/Program.cs
+++ 2taskexception/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -19,8 +20,17 @@ namespace _2taskexception
             foreach (string file in files)
             {
                 try 
-                { 
-                
+                {
+                    if (regexExtForImage.IsMatch(Path.GetExtension(file).TrimStart('.')))
+                    {
+                        using (Bitmap bmp = new Bitmap(file))
+                        {
+                            bmp.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                            string newName = Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file) + "-mirrored.gif");
+                            bmp.Save(newName);
+                            Console.WriteLine($"Mirrored file {newName}");
+                        }
+                    }
                 }
                 catch (Exception ex)
                 { 
